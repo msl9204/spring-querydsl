3c8a5a8ea3a9b6465d3b9fd5dc3c3e2aa9a97f07
minsoo.lee <letomok@gmail.com>
2022-07-20 11:39:05 +0900

    project init

---
+++ src/test/java/study/querydsl/SpringQuerydslApplicationTests.java
@@ -0,0 +1,41 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class SpringQuerydslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QHello qHello = QHello.hello; // 자기 자신을 alias로 만들어둔게 있어서 이거 써도 됨
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
